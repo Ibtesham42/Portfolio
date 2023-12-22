93221f034a33b1080e1a7fa6d4333ac286003d69
Ibtesham Akhtar <ibteshamakhtar1@gmail.com>
2023-12-23 02:54:28 +0530

    done

--- script.js
+++ script.js
@@ -15,3 +15,31 @@ function opentab(tabname){
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
 }
+let sidemenu = document.getElementById("sidemenu");
+
+function openmenu(){
+    sidemenu.style.right = "0";   //Open Menu
+}
+
+function closemenu(){
+    sidemenu.style.right = "-200px";  // Close MEnu
+}
+
+// contact form
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbz6XUAGMCua4kP9xW48ccwefvH1LQ7RoUvnUWAJY3ZMTymqWwE8xMU2epfWeCDlWH_c/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg");
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        msg.innerHTML = "Message Sent Sucessfully"
+        setTimeout(function(){
+            msg.innerHTML = " "
+        },5000)
+        form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
